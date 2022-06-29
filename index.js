12174f7e4b534d56c6618fb387863d7755d3bfc3
rithikrajyt <rrpandey8055@gmail.com>
2022-06-29 18:01:26 +0530

    Completed Drum Kit website with all  features

--- index.js
+++ index.js
@@ -1,6 +1,60 @@
-document.querySelector("button").addEventListener("click",handleClick);
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+// Detecting Click on the image
 
-function handleClick() {
-    var audio = new Audio('sounds/tom-1.mp3');
-    audio.play();
- }
+for (var i = 0; i < numberOfDrumButtons; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    var buttonInnerHTML = this.innerHTML;
+    makeSound(buttonInnerHTML); 
+    buttonAnimation(buttonInnerHTML);
+});
+}
+
+//Detecting Button press on the key
+document.addEventListener("keydown",function(event){
+    makeSound(event.key);
+    buttonAnimation(event.key);
+});
+
+//Making sound from the press or click
+function makeSound(key) {
+    switch (key) {
+        case "w":
+          var audio = new Audio("sounds/tom-1.mp3");
+          audio.play();
+          break;
+        case "a":
+          var audio = new Audio("sounds/tom-2.mp3");
+          audio.play();
+          break;
+        case "s":
+          var audio = new Audio("sounds/tom-3.mp3");
+          audio.play();
+          break;
+        case "d":
+          var audio = new Audio("sounds/tom-4.mp3");
+          audio.play();
+          break;
+        case "j":
+          var audio = new Audio("sounds/crash.mp3");
+          audio.play();
+          break;
+        case "k":
+          var audio = new Audio("sounds/kick-bass.mp3");
+          audio.play();
+          break;
+        case "l":
+          var audio = new Audio("sounds/snare.mp3");
+          audio.play();
+          break;
+        default: console.log(buttonInnerHTML);
+          break;
+      }
+    }
+function buttonAnimation(currentkey) {
+    var activeButton = document.querySelector("."+currentkey);
+    activeButton.classList.add("pressed");
+    
+    setTimeout(function(){
+        activeButton.classList.remove("pressed");
+    },100);
+}
